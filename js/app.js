dde700a6e0dfa0fa00e05ef818af3ebbebcee711
Gustavo <paredes.ga0610@gmail.com>
2021-04-23 17:07:46 -0500

    version2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //MOSTRAR MENU
-
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
@@ -24,27 +23,6 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener('click', linkAction));
-
-////////////////////////////SCROLL SECTION ACTIVE
-
-/* const sections = document.querySelectorAll("section[id]");
-
-function scrollActive() {
-  const scrollY = window.pageYOffset
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id")
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-    } else {
-      document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
-    }
-  });
-}
-window.addEventListener('scroll', scrollActive); */
 
 
 //CAMBIAR FONDO DE HEADER
@@ -102,16 +80,16 @@ themeButton.addEventListener('click',()=>{
 
 /* ////////////////////////////////ANIMACION DE SCROLL*/
 
-const sr = ScrollReveal({
+/* const sr = ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration : 1000,
   reset: true
 });
 
-sr.reveal(`.home__data, .home__img , .about__data , .about__img , .tienda__content, .contact__data, .contact__button , .footer__content`,{
+sr.reveal(`.home__img , .about__data , .about__img , .contact__data, .contact__button `,{
   interval:200
-})
+}) */
 
 /* ////////////////////////////////PAGAR*/
 let products = [];
@@ -126,3 +104,32 @@ function add(product,price){
 function pay(){
   window.alert(products.join(","))
 }
+
+
+/////////BOTONES DE TIENDA
+
+bts = document.querySelector('.bts');
+memes = document.querySelector('.memes');
+todas = document.querySelector('.todas');
+tienda__container = document.querySelector(".tienda__container");
+tienda__container2 = document.querySelector(".tienda__container2");
+
+bts.addEventListener('click',()=>{
+  console.log('hizo click en boton bts')
+  tienda__container2.classList.remove("activar")
+  tienda__container.classList.add("activar")
+})
+
+memes.addEventListener('click',()=>{
+  console.log('hizo click en boton memes')
+  tienda__container2.classList.add("activar")
+  tienda__container.classList.remove("activar")
+})
+
+todas.addEventListener('click',()=>{
+  console.log('hizo click en ver todo')
+  tienda__container.classList.remove("activar")
+  tienda__container2.classList.remove("activar")
+})
+
+
